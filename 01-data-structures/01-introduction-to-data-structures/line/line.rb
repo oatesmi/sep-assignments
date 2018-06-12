e645b042fd5bc3ab11e3db1bd9f5e2e7c06e9670
cddlslt <oatesmi.74@gmail.com>
2018-06-12 10:16:31 -0500

    code written, need to write answers

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -1,6 +1,7 @@
 # A line of people at an amusement park ride
 # There is a front to the line, as well as a back.
-# People may leave the line whenever they see fit and those behind them take their place.
+# People may leave the line whenever they see fit and those behind them take
+# their place.
 class Line
   attr_accessor :members
 
@@ -9,26 +10,36 @@ class Line
   end
 
   def join(person)
+    self.members.push(person)
   end
 
   def leave(person)
+    self.members.delete(person)
   end
 
   def front
+    self.members.first
   end
 
   def middle
+    mid = (self.members.length/2).floor
+    self.members[mid]
   end
 
   def back
+    self.members.last
   end
 
   def search(person)
+    if self.members.include?(person)
+      person
+    end
   end
 
   private
 
   def index(person)
+    self.members.find_index(person)
   end
 
 end
